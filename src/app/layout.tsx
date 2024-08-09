c1854aee11919d9325f48842bf922f695823f10d
myniqx <burak_okur@hotmail.com>
2024-08-09 21:36:51 +0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,44 @@
-import { MainLayout } from "@/components/MainLayout"
-import { Providers } from "@/components/Providers"
-import { getMetaData } from "@/constants/metadataBase"
-import { web } from "@/constants/web"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "slick-carousel/slick/slick-theme.css"
-import "slick-carousel/slick/slick.css"
+import { MainLayout } from "@/components/MainLayout";
+import { Providers } from "@/components/Providers";
+import { getMetaData } from "@/constants/metadataBase";
+import { Inter } from "next/font/google";
+import Script from "next/script";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = getMetaData({
   title: "Çözümünüz Doruk Akademi'de",
-})
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="tr">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16663815429"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16663815429');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Providers>
           <MainLayout>{children}</MainLayout>
         </Providers>
       </body>
     </html>
-  )
+  );
 }
