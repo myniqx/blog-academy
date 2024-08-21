1e5daca724ec88c243962a635bf078d7efda9ca7
myniqx <burak_okur@hotmail.com>
2024-08-21 21:13:07 +0300

    .

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -24,7 +24,7 @@ export const socialItems: { label: SocialLabels; icon: any; link: string }[] = [
   {
     label: "Instagram",
     icon: FaInstagram,
-    link: "https://instagram.com/dorukakademi",
+    link: "https://instagram.com/dorukakademii",
   },
   {
     label: "Youtube",
