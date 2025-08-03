499e5200f246d87b701bb75cbd0c72672d98ff34
myniqx <burak_okur@hotmail.com>
2025-08-03 18:27:05 +0200

    generateStaticParam

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,6 +1,12 @@
 import { BlogContent } from "@/components/BlogContent";
 import blogs from "@/constants/blogs/blogs.json";
 
+export async function generateStaticParams() {
+  return blogs.map((blog) => ({
+    slug: blog.slug,
+  }));
+}
+
 const Blog = ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
   const blogPost = blogs.find((blog) => blog.slug === slug);
