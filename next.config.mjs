363f73f05220087eb7b43b1dbceecfd19c0f25fb
Burak <burak_okur@hotmail.com>
2025-12-16 15:00:38 +0100

    Configure caching headers for images
    Added custom headers for image caching.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,22 @@
-/*
-/** @type {import('next').NextConfig} 
-const nextConfig = {};
-
-export default nextConfig;
-*/
-
 import removeImports from 'next-remove-imports';
 
 const removeImportsPlugin = removeImports();
 
 const config = removeImportsPlugin({
+
+  async headers() {
+    return [
+      {
+        source: '/images/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 });
 
 export default config;
